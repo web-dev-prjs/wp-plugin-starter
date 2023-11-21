6f4f16d450a143e60b00176d4593a74307576793
omidhoseini <webbmakerr@gmail.com>
2023-11-21 08:16:19 +0330

    Added "BasicAuth" class to check REST-API request for basic authentication.
    [Ticket: 20231121#100]

---
+++ includes/Core/BasicAuth.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Checks the REST-API request authentication.
+ *
+ * @package wp-plugin-starter
+ */
+
+namespace WPS\Core;
+
+use WP_REST_Response;
+
+/**
+ * BasicAuth class.
+ *
+ * @since 1.0.0
+ */
+final class BasicAuth {
+
+	/**
+	 * Wraps a function to checking authentication.
+	 *
+	 * @param $callback
+	 *
+	 * @return array|WP_REST_Response
+	 * @since 1.0.0
+	 */
+	public static function wrap_in_basic_auth( $callback ): array|WP_REST_Response {
+
+		return BasicAuth::basic_authentication( $callback );
+	}
+
+	/**
+	 * Makes basic authentication for use in APIs.
+	 *
+	 * @param null $_function
+	 *
+	 * @return array|WP_REST_Response Response of authenticating.
+	 * @since 1.0.0
+	 */
+	private static function basic_authentication( $_function ): array|WP_REST_Response {
+
+		if ( ! isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+			header( 'WWW-Authenticate: Basic realm=Private Area' );
+			header( 'HTTP/1.0 401 Unauthorized' );
+
+			$result = array(
+				"code"    => "rest_forbidden",
+				"message" => "Sorry, you need proper authentication.",
+				"data"    => array(
+					"status" => 401
+				)
+			);
+		} else {
+			if (
+				getenv( strtoupper( PLUGIN_PREFIX ) . '_CONSUMER_KEY' ) === $_SERVER['PHP_AUTH_USER']
+				&&
+				getenv( strtoupper( PLUGIN_PREFIX ) . '_CONSUMER_SECRET' ) === $_SERVER['PHP_AUTH_PW']
+			) {
+				$result = $_function;
+			} else {
+				header( 'WWW-Authenticate: Basic realm=Private Area' );
+				header( 'HTTP/1.0 401 Unauthorized' );
+
+				$result = array(
+					"code"    => "rest_forbidden",
+					"message" => "Sorry, you are not allowed to do that.",
+					"data"    => array(
+						"status" => 403
+					)
+				);
+			}
+		}
+
+		return $result;
+	}
+}
