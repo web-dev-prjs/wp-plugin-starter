7ca87297f75feb09da09e0df9b7eaf46c468dde3
omid <web.dev.prjs@gmail.com>
2024-03-09 16:29:39 +0330

    Temporarily changed "PLUGIN_ENV_FILE" constant value from ".env-example" to ".env".
    [Ticket: 20240309#100]

--- configs/constants.php
+++ configs/constants.php
@@ -12,21 +12,21 @@ if ( ! function_exists( 'get_plugins' ) ) {
 
 $plugin_data = get_plugins()[ PLUGIN_BASENAME ]; // Get all data plugin.
 
-define( 'PLUGIN_NAME', $plugin_data['Name'] ); // Define plugin name.
-define( 'PLUGIN_VERSION', $plugin_data['Version'] ); // Define plugin version.
-define( 'PLUGIN_DOMAIN', $plugin_data['TextDomain'] ); // Define plugin domain.
+define( 'PLUGIN_NAME', $plugin_data[ 'Name' ] );         // Define plugin name.
+define( 'PLUGIN_VERSION', $plugin_data[ 'Version' ] );   // Define plugin version.
+define( 'PLUGIN_DOMAIN', $plugin_data[ 'TextDomain' ] ); // Define plugin domain.
 
-const PLUGIN_PREFIX         = 'wps'; // Define plugin prefix.
-const PLUGIN_ENV_FILE       = '.env-example'; // Define the plugin ENV-file name.
-const TWIG_TEMPLATE_SUFFIX  = 'html.twig'; // Define Twig-template suffix.
-const OPTION_SECTION_SUFFIX = 'section'; // The option section suffix.
-const OPTION_GROUP_SUFFIX   = 'option_group'; // The option group suffix.
-const WITH_SUBPAGE          = 'General'; // Define the plugin dashboard sub-page title.
+const PLUGIN_PREFIX         = 'wps';                      // Define plugin prefix.
+const PLUGIN_ENV_FILE       = '.env';             // Define the plugin ENV-file name.
+const TWIG_TEMPLATE_SUFFIX  = 'html.twig';                // Define Twig-template suffix.
+const OPTION_SECTION_SUFFIX = 'section';                  // The option section suffix.
+const OPTION_GROUP_SUFFIX   = 'option_group';             // The option group suffix.
+const WITH_SUBPAGE          = 'General';                  // Define the plugin dashboard sub-page title.
 const PLUGIN_ALL_OPTIONS    = PLUGIN_PREFIX . '_options'; // Define plugin options.
 
 define( 'PLUGIN_NAMESPACE', strtoupper( PLUGIN_PREFIX ) . '\\' ); // Define plugin namespace.
 
-const PLUGIN_ASSETS    = PLUGIN_URL . 'assets/'; // Define plugin assets url.
-const PLUGIN_INCLUDES  = PLUGIN_PATH . 'includes/'; // Define a plugin includes-folder path.
+const PLUGIN_ASSETS    = PLUGIN_URL . 'assets/';     // Define plugin assets url.
+const PLUGIN_INCLUDES  = PLUGIN_PATH . 'includes/';  // Define a plugin includes-folder path.
 const PLUGIN_TEMPLATES = PLUGIN_PATH . 'templates/'; // Define a plugin templates-folder path.
 const PLUGIN_TEMPORARY = PLUGIN_PATH . 'temporary/'; // Define a plugin temporary-folder path.
